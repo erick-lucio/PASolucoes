dfea68f43c2bde2a726bb8e4acd100806dfd933d
erick lucio <ericksi@synnex.com>
2021-02-07 21:13:54 -0300

    .

--- BackEndPaSolucoes/src/Routes/Email/index.js
+++ BackEndPaSolucoes/src/Routes/Email/index.js
@@ -6,7 +6,9 @@ const defaultResponse = require('../../controlers/formatResponse')
 const emailController = require('../../controlers/email')
 
 emailRoutes.post("/email/sendteste", 
-    console.log("Passou na rota"),
+    ()=>{
+        console.log("passou nas rotar")
+    },
     emailController.sendEmail
 )
 
