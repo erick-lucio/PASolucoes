2bb8cdf054f07787bbb1dad3d44317a0464cdfde
erick lucio <ericksi@synnex.com>
2021-02-14 02:05:59 -0300

    .

--- FrontEndPaSolucoes/services/index.js
+++ FrontEndPaSolucoes/services/index.js
@@ -1,3 +1,1 @@
-//const localhost = "http://localhost:3102"
-const localhost = "http://134.122.22.83:3102"
-module.exports = localhost
+module.exports = process.env.NEXT_PUBLIC_NODE_ENV == "prod"? "http://134.122.22.83:3102":"http://localhost:3102"
