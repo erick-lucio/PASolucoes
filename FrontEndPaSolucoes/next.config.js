1c7ca8fa40789da43c458269bede8cae61255ad3
erick lucio <ericksi@synnex.com>
2021-02-14 03:07:52 -0300

    .

--- FrontEndPaSolucoes/next.config.js
+++ FrontEndPaSolucoes/next.config.js
@@ -1,2 +1,11 @@
 const withImages = require("next-images")
-module.exports = withImages({})
+const withPlugins = require('next-compose-plugins');
+const env = {NEXT_PUBLIC_NODE_ENV: process.env.NODE_ENV}
+
+
+module.exports = withPlugins([
+    [withImages({})],
+    {env:
+        {NEXT_PUBLIC_NODE_ENV: process.env.NODE_ENV}
+    }
+]);
