ee06687a58edbe6f61712a1ddeb7e782f91b9fca
erick lucio <ericksi@synnex.com>
2021-02-18 09:25:55 -0300

    .

--- FrontEndPaSolucoes/components/createAtendimento.js
+++ FrontEndPaSolucoes/components/createAtendimento.js
@@ -7,21 +7,14 @@ const userServices = require("../services/users")
 export default function createAtendimento(props) {
   const [urlData, setUrlData] = useState("")
   
-  const login = async ()=>{    
-    var login = document.getElementById("loginInput").value.trim()
-    var senha = document.getElementById("senhaInput").value.trim()
-    document.getElementById("textErrorMessage").value = "Usuario Não Encontrado!"
-    
-    
-    try {
-      var alo = await userServices.getAll()
- 
-    } catch (error) {
-      
-    }
 
-  }
+  function onlyOne(checkbox,radioName) {
+    var radios = radios = document.getElementsByName('radio_'+radioName)
 
+    radios.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}
   //props.setComponentCliente(0)
   return (
     <>
@@ -35,11 +28,82 @@ export default function createAtendimento(props) {
             <div>
               Novo Atendimento
             </div>
-            <input type="submit" className="create_atendimento_cancel" onClick={()=>props.setShowAtendimentoPopUp(0)} value="Cancelar"></input>
-
+            <input type="submit" className="create_atendimento_cancel" onClick={()=>props.setShowAtendimentoPopUp(0)} value="Fechar"></input>
           </div>
-          <div >
-
+          <div className="create_atendimento_content">
+            <form>
+              <div className="create_atendimento_content_cliente_div">
+                <label>Cliente:</label>
+                <div className="create_atendimento_content_radio_div">
+                  <input type="radio" onClick={onlyOne(this,'cliente')} name="radio_cliente"></input>
+                  <label>Cliente 1</label>
+                </div>
+                <div className="create_atendimento_content_radio_div">
+                  <input type="radio" onClick={onlyOne(this,'cliente')} name="radio_cliente"></input>
+                  <label>Cliente 1</label>
+                </div>                
+              </div>
+              <div className="create_atendimento_content_atendimento_div">
+                <label>Tipo de Atendimento:</label>
+                <div className="create_atendimento_content_radio_div">
+                  <input type="radio" onClick={onlyOne(this,'atendimento')} name="radio_atendimento"></input>
+                  <label>Atendimento</label>
+                </div> 
+                <div className="create_atendimento_content_radio_div">
+                  <input type="radio" onClick={onlyOne(this,'atendimento')} name="radio_atendimento"></input>
+                  <label>Atendimento</label>
+                </div> 
+                <div className="create_atendimento_content_radio_div">
+                  <input type="radio" onClick={onlyOne(this,'atendimento')} name="radio_atendimento"></input>
+                  <label>Atendimento</label>
+                </div> 
+              </div>
+              <div className="create_atendimento_content_prioridade_div">
+                <label>Prioridade:</label>
+                <div className="create_atendimento_content_radio_div">
+                  <input type="radio" onClick={onlyOne(this,'prioridade')} name="radio_prioridade"></input>
+                  <label>Normal</label>
+                </div> 
+                <div className="create_atendimento_content_radio_div">
+                  <input type="radio" onClick={onlyOne(this,'prioridade')} name="radio_prioridade"></input>
+                  <label>Normal</label>
+                </div> 
+              </div>
+              <div className="create_atendimento_content_contrato_div">
+                <label>Tipo Contrato</label> 
+                <div className="create_atendimento_content_radio_div">
+                  <input type="radio" onClick={onlyOne(this,'contrato')} name="contrato"></input>
+                  <label>Normal</label>
+                </div> 
+                <div className="create_atendimento_content_radio_div">
+                  <input type="radio" onClick={onlyOne(this,'contrato')} name="contrato"></input>
+                  <label>tipo 1</label>
+                </div> 
+                <div className="create_atendimento_content_radio_div">
+                  <input type="radio" onClick={onlyOne(this,'contrato')} name="contrato"></input>
+                  <label>tipo 2</label>
+                </div> 
+                {true?"Valor avulso":""}
+              </div>
+              <div className="create_atendimento_content_sistema_div">
+                <label>Tipo sistema</label>
+                <div className="create_atendimento_content_radio_div">
+                  <input type="radio" onClick={onlyOne(this,'sistema')} name="sistema"></input>
+                  <label>Sistema 1</label>
+                </div> 
+                <div className="create_atendimento_content_radio_div">
+                  <input type="radio" onClick={onlyOne(this,'sistema')} name="sistema"></input>
+                  <label>Sistema 2</label>
+                </div> 
+              </div>
+              <div className="create_atendimento_content_pendencia_financeira_div">
+                <label>Pendencia financeira</label>
+                <input type="text" value=""></input>
+              </div>              
+            </form>
+          </div>
+          <div className="create_atendimento_create_button_div">
+            <input type="submit" value="Criar Atendimento" className="create_atendimento_create_button"></input>
           </div>
         </div>
       </div>
